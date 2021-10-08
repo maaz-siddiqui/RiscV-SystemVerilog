2cb9772de10ec3ecfe4810a7c4e6bb3f9c7d2304
maaz-siddiqui <maaz.siddiqui3073@gmail.com>
2021-10-08 23:19:07 +0500

    type decode fully working

--- TypeDecode/typeDecode.v
+++ TypeDecode/typeDecode.v
@@ -6,29 +6,118 @@ module typeDecode (
 );
   
 always @(*) begin
-  if(!rst) begin
-  if (opcode == 7'b0000011 | opcode == 7'b0001111 | opcode == 7'b0010011) begin
-        assign I = 1;
-        assign L = 0;
-        assign Jr = 0;
-        assign S = 0;
-        assign Sb = 0;
-        assign aui = 0;
-        assign lui = 0;
-        assign J = 0;
-    end
-  else begin
-    	R <= 0;
-        assign I = 0;
-        assign L = 0;
-        assign Jr = 0;
-        assign S = 0;
-        assign Sb = 0;
-        assign aui = 0;
-        assign lui = 0;
-        assign J = 0;
-  end
-end    
+    if(!rst) begin
+      	if (opcode == 7'h13)begin
+            assign R = 0;
+            assign I = 1;
+            assign L = 0;
+            assign Jr = 0;
+            assign S = 0;
+            assign Sb = 0;
+            assign aui = 0;
+            assign lui = 0;
+            assign J = 0;
+        end
+      	else if (opcode == 7'h03 )begin
+            assign R = 0;
+            assign I = 1;
+            assign L = 1;
+            assign Jr = 0;
+            assign S = 0;
+            assign Sb = 0;
+            assign aui = 0;
+            assign lui = 0;
+            assign J = 0;
+        end
+      	else if (opcode == 7'h67)begin
+            assign R = 0;
+            assign I = 1;
+            assign L = 0;
+            assign Jr = 1;
+            assign S = 0;
+            assign Sb = 0;
+            assign aui = 0;
+            assign lui = 0;
+            assign J = 0;
+        end
+        else if (opcode == 7'h33 | opcode == 7'h3b) begin
+            assign R = 1;
+            assign I = 0;
+            assign L = 0;
+            assign Jr = 0;
+            assign S = 0;
+            assign Sb = 0;
+            assign aui = 0;
+            assign lui = 0;
+            assign J = 0;
+        end
+      	else if (opcode == 7'h23 ) begin
+            assign R = 0;
+            assign I = 0;
+            assign L = 0;
+            assign Jr = 0;
+            assign S = 1;
+            assign Sb = 0;
+            assign aui = 0;
+            assign lui = 0;
+            assign J = 0;
+        end
+      	else if (opcode == 7'h63) begin
+            assign R = 0;
+            assign I = 0;
+            assign L = 0;
+            assign Jr = 0;
+            assign S = 0;
+            assign Sb = 1;
+            assign aui = 0;
+            assign lui = 0;
+            assign J = 0;
+        end
+      	else if (opcode == 7'h17) begin
+            assign R = 0;
+            assign I = 0;
+            assign L = 0;
+            assign Jr = 0;
+            assign S = 0;
+            assign Sb = 0;
+            assign aui = 1;
+            assign lui = 0;
+            assign J = 0;
+        end
+     	 else if (opcode == 7'h37) begin
+            assign R = 0;
+            assign I = 0;
+            assign L = 0;
+            assign Jr = 0;
+            assign S = 0;
+            assign Sb = 0;
+            assign aui = 0;
+            assign lui = 1;
+            assign J = 0;
+        end
+     	else if (opcode == 7'h6f) begin
+            assign R = 0;
+            assign I = 0;
+            assign L = 0;
+            assign Jr = 0;
+            assign S = 0;
+            assign Sb = 0;
+            assign aui = 0;
+            assign lui = 0;
+            assign J = 1;
+        end
+        else begin
+            assign R = 0;
+            assign I = 0;
+            assign L = 0;
+            assign Jr = 0;
+            assign S = 0;
+            assign Sb = 0;
+            assign aui = 0;
+            assign lui = 0;
+            assign J = 0;
+        end
+    end    
 end
 
 endmodule
--- TypeDecode/typeDecode_tb.v
+++ TypeDecode/typeDecode_tb.v
@@ -28,8 +28,22 @@ initial begin
   	rst_tb=1;
   	#5
   	rst_tb=0;
-    opcode_tb = 7'b0001111;       
+  	#5
+    opcode_tb = 7'h33;       
     #5
+    opcode_tb = 7'h13;
+ 	#5
+    opcode_tb = 7'h03;
+  	#5
+    opcode_tb = 7'h67; 
+  	#5
+    opcode_tb = 7'h23;
+  	#5
+    opcode_tb = 7'h63;
+  	#5
+    opcode_tb = 7'h173;
+  	#5
+    opcode_tb = 7'h6f;
     #40
     $finish;
     end
