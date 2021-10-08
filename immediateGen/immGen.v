337503b2f9d709ba6c0795425a362e6eab571502
maaz-siddiqui <maaz.siddiqui3073@gmail.com>
2021-10-09 01:12:37 +0500

    S immediate

--- immediateGen/immGen.v
+++ immediateGen/immGen.v
@@ -6,18 +6,25 @@ module immGen(
 );
 
   reg [19:0]temp1;
-  reg [11:0]temp2;  
+  reg [11:0]temp2; 
+  reg [6:0]temp7 ;
+  reg [4:0]temp5;
   reg [31:0]zeros = 32'd0;
 
   
 always @(*) begin
   if(!rst) begin
-    assign {temp2, temp1} = Ins;
-    if(Ins[31] == 1) 
-      assign temp1 = 20'h11111;
+    //I type
+    assign {temp2, temp1} = Ins; //divides inst to two array
+    if(Ins[31] == 1)  //if 31st bit 1 then extend all ones
+      assign temp1 = 20'hfffff;
     else 
       assign temp1 = 20'd0;	
     assign I_imm = {temp1 ,temp2};
+    //S type
+    temp7 = Ins[25+:31];
+    temp5 = Ins[7+:11];
+    assign S_imm = {temp1,temp7 ,temp5 };
   end
   else begin 
     assign I_imm = {zeros};    
