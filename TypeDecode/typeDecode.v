0093602936a939dab12b3f040625c1c009b11f5c
maaz-siddiqui <maaz.siddiqui3073@gmail.com>
2021-10-08 22:40:40 +0500

    I type working

--- TypeDecode/typeDecode.v
+++ TypeDecode/typeDecode.v
@@ -1,13 +1,34 @@
 // Code your design here
 module typeDecode (
-    input logic [6:0 ]opcode,
+  	input logic [6:0 ]opcode,
+  	input logic rst,
     output logic R, I, L, Jr, S, Sb, aui, lui, J
 );
   
 always @(*) begin
- 	if (opcode == 0000011 || opcode == 0001111 || opcode == 0010011) begin
+  if(!rst) begin
+  if (opcode == 7'b0000011 | opcode == 7'b0001111 | opcode == 7'b0010011) begin
         assign I = 1;
+        assign L = 0;
+        assign Jr = 0;
+        assign S = 0;
+        assign Sb = 0;
+        assign aui = 0;
+        assign lui = 0;
+        assign J = 0;
     end
+  else begin
+    	R <= 0;
+        assign I = 0;
+        assign L = 0;
+        assign Jr = 0;
+        assign S = 0;
+        assign Sb = 0;
+        assign aui = 0;
+        assign lui = 0;
+        assign J = 0;
+  end
 end    
+end
 
 endmodule
--- TypeDecode/typeDecode_tb.v
+++ TypeDecode/typeDecode_tb.v
@@ -6,8 +6,8 @@ initial begin
 	$dumpvars;
 end 
 
-logic [6:0 ]opcode_tb,
-logic R_tb, I_tb, L_tb, Jr_tb, S_tb, Sb_tb, aui_tb, lui, J
+logic [6:0 ]opcode_tb;
+logic R_tb, I_tb, L_tb, Jr_tb, S_tb, Sb_tb, aui_tb, lui_tb, J_tb, rst_tb;
 
 typeDecode typeDecode1(
   .R(R_tb),
@@ -18,11 +18,16 @@ typeDecode typeDecode1(
   .Sb(Sb_tb),
   .aui(aui_tb),
   .lui(lui_tb),
-  .J(J_tb)
+  .J(J_tb),
+  .rst(rst_tb),
+  .opcode(opcode_tb)
 );
 
 
 initial begin
+  	rst_tb=1;
+  	#5
+  	rst_tb=0;
     opcode_tb = 7'b0001111;       
     #5
     #40
