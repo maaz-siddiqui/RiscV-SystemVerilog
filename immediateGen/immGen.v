ca655aa933a18e6c8c602c6159bbb8e760531530
maaz-siddiqui <maaz.siddiqui3073@gmail.com>
2021-10-08 23:25:05 +0500

    working on Immediate generation

---
+++ immediateGen/immGen.v
@@ -0,0 +1,13 @@
+// Code your design here
+module immGen(
+  	input logic [31:0] opA, [31:0] opB,
+    input logic [3:0 ]aluOutSel,
+    output logic [31:0] aluOut
+);
+  
+	int i = 0;
+always @(*) begin
+ 	
+end    
+
+endmodule
